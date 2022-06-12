fd14b062f4ae15358b377b02aab9056acd7590b3
Akash Mishra <akashmishra.akm@gmail.com>
2022-06-12 22:02:19 +0530

    Added path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const Sequelize = require("sequelize");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
